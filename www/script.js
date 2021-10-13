78fcbf13f05fed44a88d1a406f304816ce5efb2b
giacomo-ascari <giacomolego@gmail.com>
2021-10-13 16:46:47 +0200

    fix: buttons now with style

--- www/script.js
+++ www/script.js
@@ -2,7 +2,7 @@ function changeColor() {
     let hue = Math.floor(Math.random() * 360);
     let color = "hsl(" + String(parseInt(hue)) + ",33%,20%)"
     document.getElementById("_body").style.backgroundColor = color;
-    let elements = document.getElementsByClassName("_a");
+    let elements = document.getElementsByTagName("button");
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.color = color;
     }
